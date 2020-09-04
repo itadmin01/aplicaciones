27f8e597bba0f3764fb12b36da0f2d25bfbfa68e
IT Admin <admin@itadmin.com.mx>
2020-09-04 13:32:39 -0500

    [ADD] Biometriz XK and nomina bancos

---
+++ nomina_cfdi_bancos/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Nomina CFDI bancos",
+    "author": "IT Admin",
+    "version": "13.1",
+    "category": "Other",
+    "description":"Genera dispersion de bancos para la nómina.",
+    "depends": ["nomina_cfdi_ee"],
+    "data": [
+        "wizard/generar_pagos_banco.xml",
+        "views/hr_employee_view.xml",
+    ],
+    "license": 'AGPL-3',
+    'installable': True,
+    'images': [''],
+}
